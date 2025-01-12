12fd448385a269fe6f4f1a6e6f6ca78b56516430
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2025-01-13 02:00:06 +0530

    made setup for stripe

--- src/components/global/billing/index.tsx
+++ src/components/global/billing/index.tsx
@@ -1,26 +1,24 @@
-"use client";
-import React from "react";
-import PaymentCard from "./payment-card";
+'use client'
+import React from 'react'
+import PaymentCard from './payment-card'
+import { useQueryUser } from '@/hooks/user-queries'
 
-type Props = {};
+type Props = {}
 
 const Billing = (props: Props) => {
-  // Static data
-  const data: { subscription: { plan: "PRO" | "FREE" } } = {
-    subscription: {
-      plan: "FREE", // Swap the plan to FREE
-    },
-  };
-
+  const { data } = useQueryUser()
   return (
     <div className="flex lg:flex-row flex-col gap-5 w-full lg:w-10/12 xl:w-8/12 container">
-      {/* FREE Plan */}
-      <PaymentCard current={data.subscription.plan} label="FREE" />
-
-      {/* PRO Plan */}
-      <PaymentCard current={data.subscription.plan} label="PRO" />
+      <PaymentCard
+        current={data?.data?.subscription?.plan!}
+        label="PRO"
+      />
+      <PaymentCard
+        current={data?.data?.subscription?.plan!}
+        label="FREE"
+      />
     </div>
-  );
-};
+  )
+}
 
-export default Billing;
+export default Billing
