191ca252ddb678df56a77c8cc8750c924fbab611
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2025-01-23 19:33:58 +0530

    .

--- src/app/(protected)/callback/instagram/page.tsx
+++ src/app/(protected)/callback/instagram/page.tsx
@@ -1,24 +1,35 @@
-import { onIntegrate } from '@/actions/integrations'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import { onIntegrate } from '@/actions/integrations';
+import { redirect } from 'next/navigation';
+import { useSearchParams } from 'next/navigation'; 
+import React, { useEffect } from 'react';
 
-type Props = {
-  searchParams: {
-    code: string
-  }
-}
+const Page = () => {
+  const [searchParams] = useSearchParams(); 
+  const code = searchParams.get('code'); 
 
-const Page = async ({ searchParams: { code } }: Props) => {
-  if (code) {
-    console.log(code)
-    const user = await onIntegrate(code.split('#_')[0])
-    if (user.status === 200) {
-      return redirect(
-        `/dashboard/${user.data?.firstname}${user.data?.lastname}/integrations`
-      )
-    }
-  }
-  return redirect('/sign-up')
-}
+  useEffect(() => {
+    const handleIntegration = async () => {
+      if (code) {
+        console.log(code);
+        const user = await onIntegrate(code.split('#_')[0]);
+        if (user.status === 200) {
+          redirect(
+            `/dashboard/${user.data?.firstname}${user.data?.lastname}/integrations`
+          ); 
+        } else {
+          // Handle integration errors (e.g., redirect to an error page)
+          redirect('/error'); 
+        }
+      } else {
+        // Handle missing 'code' parameter (e.g., redirect to an error page)
+        redirect('/error'); 
+      }
+    };
 
-export default Page 
+    handleIntegration(); 
+  }, [code]); 
+
+  return null; 
+};
+
+export default Page;
