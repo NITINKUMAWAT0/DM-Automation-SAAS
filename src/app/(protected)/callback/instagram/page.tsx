edd1a1aa4cb226a6ac5a1fe0d784a560a21aaabb
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2025-01-23 19:44:09 +0530

    .

--- src/app/(protected)/callback/instagram/page.tsx
+++ src/app/(protected)/callback/instagram/page.tsx
@@ -1,37 +1,26 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { onIntegrate } from '@/actions/integrations';
-import { useRouter } from 'next/router';
-import { useSearchParams } from 'next/navigation'; 
-import React, { useEffect } from 'react';
+import { redirect } from 'next/navigation';
+import React from 'react';
 
-const Page = () => {
-  const searchParams = useSearchParams(); 
-  const router = useRouter();
-  const code = searchParams.get('code'); 
+type Props = {
+  searchParams: {
+    code: string;
+  };
+};
 
-  useEffect(() => {
-    const handleIntegration = async () => {
-      if (code) {
-        console.log(code);
-        const user = await onIntegrate(code.split('#_')[0]);
-        if (user.status === 200) {
-          router.push(
-            `/dashboard/${user.data?.firstname}${user.data?.lastname}/integrations`
-          ); 
-        } else {
-          // Handle integration errors (e.g., redirect to an error page)
-          router.push('/error'); 
-        }
-      } else {
-        // Handle missing 'code' parameter (e.g., redirect to an error page)
-        router.push('/error'); 
-      }
-    };
-
-    handleIntegration(); 
-  }, [code, router, code.split('#_')[0]]); 
-
-  return null; 
+const Page = async ({ searchParams: { code } }: Props) => {
+  if (code) {
+    console.log(code);
+    const user = await onIntegrate(code.split('#_')[0]);
+    if (user.status === 200) {
+      redirect(
+        `/dashboard/${user.data?.firstname}${user.data?.lastname}/integrations`
+      );
+      return null;  // Make sure to return null after redirection
+    }
+  }
+  redirect('/sign-up');
+  return null;  // Make sure to return null after redirection
 };
 
 export default Page;
