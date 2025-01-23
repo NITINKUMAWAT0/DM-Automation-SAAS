f52eea25576c41ef9050534c1ab6a3802e30863a
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2025-01-23 19:38:24 +0530

    .

--- src/app/(protected)/callback/instagram/page.tsx
+++ src/app/(protected)/callback/instagram/page.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { onIntegrate } from '@/actions/integrations';
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/router';
 import { useSearchParams } from 'next/navigation'; 
 import React, { useEffect } from 'react';
 
 const Page = () => {
-  const [searchParams] = useSearchParams(); 
+  const searchParams = useSearchParams(); 
+  const router = useRouter();
   const code = searchParams.get('code'); 
 
   useEffect(() => {
@@ -13,21 +15,21 @@ const Page = () => {
         console.log(code);
         const user = await onIntegrate(code.split('#_')[0]);
         if (user.status === 200) {
-          redirect(
+          router.push(
             `/dashboard/${user.data?.firstname}${user.data?.lastname}/integrations`
           ); 
         } else {
           // Handle integration errors (e.g., redirect to an error page)
-          redirect('/error'); 
+          router.push('/error'); 
         }
       } else {
         // Handle missing 'code' parameter (e.g., redirect to an error page)
-        redirect('/error'); 
+        router.push('/error'); 
       }
     };
 
     handleIntegration(); 
-  }, [code]); 
+  }, [code, router, code.split('#_')[0]]); 
 
   return null; 
 };
